bf44d4e2e1164583b449e149b14f9fb4e6bb318c
Xuan Trung <xuantrung276@gmail.com>
2023-06-30 16:24:48 +0700

    change update product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -140,8 +140,7 @@ class ProductController extends Controller
     public function update(ProductFormRequest $request, int $product_id)
     {
         $validatedData = $request->validated();
-        $product = Category::findOrFail($validatedData['category_id'])
-            ->products()->where('id', $product_id)->first();
+        $product = Product::where('id', $product_id)->first();
 
         if ($product) {
             $product->update([
