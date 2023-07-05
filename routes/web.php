79e1310d01a96fbba791a3405d50e31095b8ab79
Xuan Trung <xuantrung276@gmail.com>
2023-07-06 00:43:33 +0700

    Delete/remove WishList items using Livewire

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,9 @@ Route::prefix('/')->group(function () {
     Route::get('collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products']);
     Route::get('collections/{category_slug}/{product_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'productView']);
 
-    Route::get('wishlist', [App\Http\Controllers\Frontend\WishlistController::class, 'index']);
+    Route::middleware(['auth'])->group(function () {
+        Route::get('wishlist', [App\Http\Controllers\Frontend\WishlistController::class, 'index']);
+    });
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
